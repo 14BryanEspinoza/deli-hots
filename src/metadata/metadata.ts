eed44928e294e73d2ac4fda34814572982170231
Bryan Espinoza <14bryespinoza@gmail.com>
2026-09-16 13:48:04 -0600

    feat: add favicon, metadate SEO & sitemap and custom properties

---
+++ src/metadata/metadata.ts
@@ -0,0 +1,20 @@
+interface MetaProps {
+  description: string;
+  keywords: string;
+  author: string;
+  title: string;
+  image: string;
+  canonical: string;
+}
+
+export const meta: MetaProps = {
+  description:
+    "Deli's Hots – fast food with fresh ingredients. Burgers, pizzas, hot dogs, tacos, and more, ready in minutes.",
+  keywords:
+    "Fast food, Hamburgers, Pizzas, Hot dogs, Tacos, Deli-style hot dogs",
+  author: "14BryanEspinoza",
+  title: "Deli's Hots",
+  image:
+    "https://raw.githubusercontent.com/14BryanEspinoza/delis-hot/main/public/preview.png",
+  canonical: "https://14bryanespinoza.github.io/delis-hot/",
+};
